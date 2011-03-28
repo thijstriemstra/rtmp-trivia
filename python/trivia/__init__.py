d558c52c477048ff46f68df691235f2b5e18575b
Thijs Triemstra <info@collab.nl>
2011-03-28 23:32:04 +0200

    add remoting call for questions at startup.

--- python/trivia/__init__.py
+++ python/trivia/__init__.py
@@ -7,12 +7,14 @@ Trivia game.
 
 
 import datetime
+import logging
 
 from twisted.python import log
 
 from rtmpy.server import Application, Client
 
 from pyamf.versions import Version
+from pyamf.remoting import RemotingError
 from pyamf.remoting.client import RemotingService
 
 from sqlalchemy.sql import select, and_
@@ -48,22 +50,15 @@ class TriviaClient(Client):
         return data
 
 
-    def getQuestions(self):
-        """
-        Get all Trivia questions.
-        """
-        return ['todo']
-
-
 class TriviaApplication(Application):
     """
     Trivia server-side application.
     """
 
     client = TriviaClient
-    host = 'collab.dev/gateway'
-    address = 'http://%s' % (host)
-    appAgent = 'CollabTrivia/1.0.0'
+    gateway = 'http://trivia.dev/gateway'
+    service_path = 'trivia'
+    appAgent = 'CollabTrivia/%s' % str(__version__)
     questions = None
     newQuestion_Interval = 4000
     hint_interval = 12000
@@ -72,43 +67,58 @@ class TriviaApplication(Application):
     totalNrQuestions = 26380
     startup_time = 0
 
+    def onConnect(self, client):
+        """
+        """
+        log.msg( "Accepted connection for '%s' from client: %s" % (self.name, client.id))
+        log.msg( "Flash Player: %s" % client.agent )
+        log.msg( "URI: %s" % client.uri )
+
+        return True
+
+
+    def onDisconnect(self, client):
+        """
+        """
+        print "Client '%s' has been disconnected from the application" % client.id
+
 
     def onAppStart(self):
         """
         @note `onAppStart` is not fully implemented in RTMPy, see #138
         """
-        #server = self.module_context.getServer()
         #started = datetime.fromtimestamp(server.getStartTime() / 1000)
 
         log.msg(60 * "=")
         #log.msg("Server version: %s." % server.getVersion())
         #log.msg("Server started on: %s" % started)
-        log.msg("Trivia Gateway: %s" % self.address)
+        log.msg("Trivia Gateway: %s" % self.gateway)
         log.msg(60 * "=")
-        
+
         # create Mr. Trivia
         if self.name == "trivia":
             # XXX: no shared object support yet (#46)
-            #self.questions = self.getAttribute("askedQuestions")
+            self.questions = [] #self.getAttribute("askedQuestions")
+
+            # create AMF client
+            self.remoting = RemotingService(self.gateway, logger=logging,
+                                            user_agent=self.appAgent)
+            self.service = self.remoting.getService(self.service_path)
 
             # is it the first time
-            if self.questions == None:
+            if len(self.questions) == 0:
                 # load the questions from database
                 print("TRIVIA : Loading Trivia questions...")
 
-                # create AMF client
-                #self.service_path = trivia_namespace
-                #self.client = RemotingService(self.address, self.service_path,
-                #    logger=log, user_agent=self.appAgent)
-
-                # invoke a method on the server using an AMF client.
-                #self.questions = self.client.getQuestions()
+                try:
+                    # load startup questions
+                    self.questions = self.service.getQuestions()
+                except RemotingError, e:
+                    raise Exception(e)
 
                 # save the array in sharedobject (#46)
                 #self.setAttribute("askedQuestions", self.questions)
             else:
-                self.questions = self.questions.getValue()
-
                 # start Mr. Trivia
                 self._start_game();
 
@@ -126,28 +136,14 @@ class TriviaApplication(Application):
             log.msg("Started Mr. Trivia with %s questions." % len(self.questions))
 
             # load response record data
-            #application.responseTimeRecord = result[1].items[0]
-            #application.responseTimeRecord.username = result[2].items[0].username
+            #self.responseTimeRecord = result[1].items[0]
+            #self.responseTimeRecord.username = result[2].items[0].username
 
-            #traceMsg("Current world record by '%s", 3, application.responseTimeRecord.username + "' with " + application.responseTimeRecord.responseTime + " seconds.", 38)
+            #traceMsg("Current world record by '%s", 3,
+            #         self.responseTimeRecord.username + "' with " +
+            #         self.responseTimeRecord.responseTime + " seconds.", 38)
 
             # start with first question
-            self._next_question()
+            #self._next_question()
         else:
             log.err("Error loading questions! Returned: %s" % (str(self.questions)))
-
-
-    def onConnect(self, client):
-        """
-        """
-        log.msg( "Accepted connection for '%s' from client: %s" % (self.name, client.id))
-        log.msg( "Flash Player: %s" % client.agent )
-        log.msg( "URI: %s" % client.uri )
-
-        return True
-
-
-    def onDisconnect(self, client):
-        """
-        """
-        print "Client '%s' has been disconnected from the application" % client.id
